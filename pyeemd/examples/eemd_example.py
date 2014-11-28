c1b9add2126118ca8a0f818cb9bab1ef81b8dd76
Perttu Luukko <perttu.luukko@iki.fi>
2014-11-28 15:16:14 +0200

    pyeemd: add a new, less-trivial example

--- pyeemd/examples/eemd_example.py
+++ pyeemd/examples/eemd_example.py
@@ -18,13 +18,35 @@
 # along with libeemd.  If not, see <http://www.gnu.org/licenses/>.
 
 from pyeemd import eemd
-from pyeemd.utils import plot_imfs
-from pylab import plot, show
-from numpy import arange, cos, pi
+from pylab import plot, show, title, figure
+import numpy as np
+from numpy import pi
 
-x = arange(0, 512)
-y = cos(2*pi/30*x) + cos(2*pi/34*x)
+# An example signal with a lower frequency sinusoid modulated by an
+# intermittent higher-frequency sinusoid
+x = np.linspace(0, 2*pi, num=500)
+signal = np.sin(4*x)
+intermittent = 0.1*np.sin(80*x)
+y = signal * (1 + np.select([signal > 0.7], [intermittent]))
+
+figure()
+title("Original signal")
 plot(x, y)
+
+# Decompose with EEMD
 imfs = eemd(y, num_siftings=10)
-plot_imfs(imfs)
+
+# Plot high-frequency IMFs (1-3) and the rest separately. This illustrates how EEMD can extract the intermittent signal.
+
+highfreq_sum = np.sum([ imfs[i] for i in range(0,3) ], axis=0)
+lowfreq_sum = np.sum([ imfs[i] for i in range(3, imfs.shape[0]) ], axis=0)
+
+figure()
+title("Sum of IMFs 1 to 3")
+plot(x, highfreq_sum)
+
+figure()
+title("Sum of rest of IMFs")
+plot(x, lowfreq_sum)
+
 show()
